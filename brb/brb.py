4ce037d24938a403f4ec609a95e9e179b7552cb7
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2020-07-16 14:07:21 +0200

    format code; add module docstring with example

--- brb/brb.py
+++ brb/brb.py
@@ -1,4 +1,34 @@
-from typing import List, Dict, Tuple, Any
+"""BRB RIMER implementation.
+
+Based on "Belief Rule-Base Inference Methodology Using the Evidential Reasoning
+Approach - RIMER", by _Yang et al._, this module implements three data
+structures to construct expert systems based on the Evidential Reasoning
+approach.
+
+    Typical usage example:
+
+    >>> from brb.brb import RuleBaseModel, Rule, AttributeInput
+    >>> model = RuleBaseModel(U=['Antecedent'], A={'Antecedent': ['good', 'bad']}, D=['good', 'bad'])
+    >>> model.add_rule(Rule(
+    ...     A_values={'Antecedent':'good'},
+    ...     delta={'Antecedent':1},
+    ...     theta=1,
+    ...     beta=[1,0]
+    ... ))
+    >>> model.add_rule(Rule(
+    ...     A_values={'Antecedent':'bad'},
+    ...     delta={'Antecedent':1},
+    ...     theta=1,
+    ...     beta=[0,1]
+    ... ))
+    >>> X = AttributeInput({'Antecedent': {'good': 1, 'bad': 0}})
+    >>> model.run(X)
+    [1.0, 0.0]
+    >>> X = AttributeInput({'Antecedent': {'good': 0.3, 'bad': 0.7}})
+    >>> model.run(X)
+    [0.15517241379310348, 0.8448275862068964]
+"""
+from typing import List, Dict, Any
 
 import numpy as np
 
@@ -9,7 +39,8 @@ class AttributeInput():
     Consists of a set of antecedent attribute values and degrees of belief.
 
     Attributes:
-        attr_input: X. Relates antecedent attributes with values and belief degrees. Must follow same order of reference values as in the model.
+        attr_input: X. Relates antecedent attributes with values and belief
+        degrees. Must follow same order of reference values as in the model.
     """
 
     def __init__(self, attr_input: Dict[str, Dict[Any, float]]):
@@ -20,19 +51,25 @@ class AttributeInput():
 class Rule():
     """A rule definition in a BRB system.
 
-    It translates expert knowledge into a mapping between the antecedents and the consequents. We assume that it is defined as a pure AND rules, that is, the only logical relation between the input attributes is the AND function.
+    It translates expert knowledge into a mapping between the antecedents and
+    the consequents. We assume that it is defined as a pure AND rules, that is,
+    the only logical relation between the input attributes is the AND function.
 
     Attributes:
-        A_values: A^k. Dictionary that matches reference values for each antecedent attribute that activates the rule.
+        A_values: A^k. Dictionary that matches reference values for each
+        antecedent attribute that activates the rule.
         delta: \delta_k. Relative weights of antecedent attributes.
         theta: \theta_k. Rule weight.
-        beta: \bar{\beta}. Expected belief degrees of consequents if rule is (completely) activated.
+        beta: \bar{\beta}. Expected belief degrees of consequents if rule is
+        (completely) activated.
     """
 
-    def __init__(self, A_values: Dict[str, Any], delta: Dict[str, float], theta: float, beta: List[float]):
+    def __init__(self, A_values: Dict[str, Any], delta: Dict[str, float],
+                 theta: float, beta: List[float]):
         self.A_values = A_values
 
-        # there must exist a weight for all antecedent attributes that activate the rule
+        # there must exist a weight for all antecedent attributes that activate
+        # the rule
         for U_i in A_values.keys():
             assert U_i in delta.keys()
         self.delta = delta
@@ -45,19 +82,27 @@ class Rule():
         """
         self._assert_input(X)
 
-        norm_delta = {attr: d / max(self.delta.values()) for attr, d in self.delta.items()}
-        weighted_alpha = [[alpha_i ** norm_delta[attr] for A_i, alpha_i in X.attr_input[attr].items() if A_i == self.A_values[attr]] for attr in self.A_values.keys()]
+        norm_delta = {attr: d / max(self.delta.values()) for attr, d
+                      in self.delta.items()}
+        weighted_alpha = [[
+                alpha_i ** norm_delta[attr] for A_i, alpha_i
+                in X.attr_input[attr].items() if A_i == self.A_values[attr]
+            ] for attr in self.A_values.keys()
+        ]
 
         return np.prod(weighted_alpha)
 
     def get_belief_degrees_complete(self, X: AttributeInput) -> Dict[Any, Any]:
-        """Returns belief degress transformed based on input completeness
+        """Returns belief degrees transformed based on input completeness
         """
         self._assert_input(X)
 
-        attribute_total_activations = {attr: sum(X.attr_input[attr].values()) for attr in X.attr_input.keys()}
+        attribute_total_activations = {attr: sum(X.attr_input[attr].values())
+                                       for attr in X.attr_input.keys()}
 
-        rule_input_completeness = sum([attribute_total_activations[attr] for attr in self.A_values.keys()]) / len(self.A_values.keys())
+        rule_input_completeness = sum([attribute_total_activations[attr]
+                                       for attr in self.A_values.keys()]) \
+                                    / len(self.A_values.keys())
 
         norm_beta = [belief * rule_input_completeness for belief in self.beta]
 
@@ -74,8 +119,9 @@ class Rule():
 
 class RuleBaseModel():
     """Parameters for the model.
-    
-    It contains the basic, standard information that will be used to manage the information and apply the operations.
+
+    It contains the basic, standard information that will be used to manage the
+    information and apply the operations.
 
     Attributes:
         U: Antecendent attributes' names.
@@ -84,7 +130,7 @@ class RuleBaseModel():
         F: ?
         rules: List of rules.
     """
-    def __init__(self, U: List[str], A: Dict[str, List[Any]], D: List[Any], F):
+    def __init__(self, U: List[str], A: Dict[str, List[Any]], D: List[Any], F=None):
         # no repeated elements for U
         assert len(U) == len(set(U))
         self.U = U
@@ -101,12 +147,14 @@ class RuleBaseModel():
     def add_rule(self, new_rule: Rule):
         """Adds a new rule to the model.
 
-        Verifies if the given rule agrees with the model settings and adds it to `.rules`.
+        Verifies if the given rule agrees with the model settings and adds it
+        to `.rules`.
         """
         # all reference values must be related to an attribute
         assert set(new_rule.A_values.keys()) == set(self.U)
 
-        # the reference values that activate the rule must be a valid referential value in the self
+        # the reference values that activate the rule must be a valid
+        # referential value in the self
         for U_i, A_i in new_rule.A_values.items():
             assert A_i in self.A[U_i]
 
@@ -127,25 +175,37 @@ class RuleBaseModel():
         alphas = [rule.get_matching_degree(X) for rule in self.rules]
 
         # 3. activation weight
-        total_theta_alpha = sum([rule.theta*alpha_k for rule, alpha_k in zip(self.rules, alphas)])
+        total_theta_alpha = sum([rule.theta*alpha_k for rule, alpha_k
+                                 in zip(self.rules, alphas)])
         # activation_weights[k] = w_k = activation weight of the k-th rule
-        activation_weights = [rule.theta * alpha_k / total_theta_alpha for rule, alpha_k in zip(self.rules, alphas)]
+        activation_weights = [rule.theta * alpha_k / total_theta_alpha
+                              for rule, alpha_k in zip(self.rules, alphas)]
 
         # 4. analytical ER algorithm
-        # the following is based on "Inference and learning methodology of belief-rule-based expert system for pipeline leak detection", _Xu et al._
+        # the following is based on "Inference and learning methodology of
+        # belief-rule-based expert system for pipeline leak detection" by
+        # _Xu et al._
         belief_degrees = [rule.beta for rule in self.rules]
         total_belief_degrees = [sum(beta_k) for beta_k in belief_degrees]
         left_prods = list()
-        for rules_beta_j in map(list, zip(*belief_degrees)):  # transpose belief degrees
+        # transpose belief degrees
+        for rules_beta_j in map(list, zip(*belief_degrees)):
             left_prods.append(np.prod(
-                [weight_k * rules_beta_jk + 1 - weight_k * total_belief_degrees_k for weight_k, rules_beta_jk, total_belief_degrees_k in zip(activation_weights, rules_beta_j, total_belief_degrees)]
+                [weight_k * rules_beta_jk + 1 - weight_k * total_belief_degrees_k  # pylint: disable=line-too-long
+                 for weight_k, rules_beta_jk, total_belief_degrees_k
+                 in zip(activation_weights, rules_beta_j, total_belief_degrees)]
             ))
         right_prod = np.prod(
-            [1 - weight_k * total_belief_degrees_k for weight_k, total_belief_degrees_k in zip(activation_weights, total_belief_degrees)]
+            [1 - weight_k * total_belief_degrees_k
+             for weight_k, total_belief_degrees_k
+             in zip(activation_weights, total_belief_degrees)]
         )
         mi = 1 / (sum(left_prods) - (len(self.D) - 1) * right_prod)
 
-        belief_degrees = [mi * (left_prod - right_prod) / (1 - mi * np.prod([1 - weight_k for weight_k in activation_weights])) for left_prod in left_prods]
+        belief_degrees = [mi * (left_prod - right_prod) / (1 - mi * \
+                          np.prod([1 - weight_k for weight_k
+                                   in activation_weights]))
+                          for left_prod in left_prods]
 
         # TODO: add utility calculation
 
