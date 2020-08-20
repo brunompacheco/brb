0b5d9d743e74997104bd92cbee0caf6d1561d5d8
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2020-08-20 13:02:46 +0200

    implement support for intervals

--- brb/brb.py
+++ brb/brb.py
@@ -161,8 +161,7 @@ class Rule():
 
     def __init__(
             self,
-            A_values: Dict[str,
-            Any],
+            A_values: Dict[str, Any],
             beta: List[float],
             delta: Dict[str, float] = None,
             theta: float = 1,
@@ -200,17 +199,56 @@ class Rule():
             referential value `A_i`.
         """
         match = 0.0
-        
-        if isinstance(X_i, type(A_i)):
-            match = float(X_i == A_i)
-        elif isinstance(X_i, dict):
-            try:
-                match = float(X_i[A_i])
-            except KeyError:
-                match = 0.0
+
+        _X_i = _prep_referential_value(X_i)
+        _A_i = _prep_referential_value(A_i)
+
+        if is_numeric(_X_i):
+            if is_numeric(_A_i):
+                match = float(_X_i == _A_i)
+            elif isinstance(_A_i, interval) or isinstance(_A_i, set):
+                match = float(_X_i in _A_i)
+        elif isinstance(_X_i, str):
+            if isinstance(_A_i, str):
+                match = float(_X_i == _A_i)
+        elif isinstance(_X_i, interval):
+            if is_numeric(_A_i):
+                # In this case, if the input covers the referential value, we
+                # consider it a match. We do so in a binary manner because it
+                # would be impossible to quantify how much of the input is
+                # covered by the referential value, as the latter has no
+                # measure.
+                match = float(_A_i in _X_i)
+            elif isinstance(_A_i, interval):
+                # For this scenario, we quantify the match as the amount of the
+                # input that is contained in the referential value.
+                intrsc = _A_i & _X_i
+                intrsc_length = intrsc[0][1] - intrsc[0][0]
+
+                _X_i_length = _X_i[0][1] - _X_i[0][0]
+
+                match = float(intrsc_length / _X_i_length)
+        elif isinstance(_X_i, set):
+            if is_numeric(_A_i):
+                # Same as the case for interval input and numeric reference.
+                match = float(_A_i in _X_i)
+            elif isinstance(_A_i, set):
+                intrsc_length = len(_X_i & _A_i)
+                _X_i_length = len(_X_i)
+
+                match = float(intrsc_length / _X_i_length)
+        elif isinstance(_X_i, dict):
+            if isinstance(_A_i, str) or is_numeric(_A_i):
+                match = float(_X_i[_A_i])
+            elif isinstance(_A_i, interval) or isinstance(_A_i, set):
+                matching_certainties = [_X_i[key] for key in _X_i.keys()
+                                        if key in _A_i]
+
+                match = float(sum(matching_certainties))
+            elif isinstance(_A_i, dict):
+                raise NotImplementedError('Uncertain rules are not supported')
         else:
-            # TODO: raise warning for mismatched types
-            pass
+            warn('Input {} mismatches the referential value {}'.format(X_i, A_i))
 
         return match
 
