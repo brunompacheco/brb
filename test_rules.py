ae6bdea067d1040d893bb36b8686e88329ea67cf
philippnoodt <philipp.noodt@ima-ifu.rwth-aachen.de>
2020-08-31 14:33:40 +0200

    created klein2019_rules.csv in test_rules.py: one can generate an individual input to test belief rule base

--- test_rules.py
+++ test_rules.py
@@ -31,10 +31,19 @@ def get_model_from_csv(csv_path: str) -> RuleBaseModel:
 
     return model
 
+def custom_input(A_i, X_i):
+    user_inputs = A_i.copy()
+    for idx, ref_val in enumerate(A_i):
+        if ref_val == X_i:
+            user_inputs[idx] = 1
+        else:
+            user_inputs[idx] = 0
+    return user_inputs
+
 curdir_path = '/Users/philippnoodt/VirtualBox_VMs/Win10/Win10_SharedFolder/MA/coding/Bruno/git/brb/'
 
 if __name__ == "__main__":
-    rules_filepath = os.path.join(os.curdir, 'eggensperger2015_rules.csv')
+    rules_filepath = os.path.join(os.curdir, 'klein2019_rules.csv')
     model = get_model_from_csv(rules_filepath)
 
     # create random test inputs using referential values identical with those in the rule base
@@ -57,10 +66,44 @@ if __name__ == "__main__":
 
         X = AttributeInput(attr_input)
         belief_degrees = model.run(X)
+        print(attr_input)
         print(tuple(zip(model.D, belief_degrees)))
 
 
     # create random test inputs using new referential values
+
+
+    # create individual test inputs
+    print('\nindividual test inputs')
+    # individual test input klein2019_rules.csv
+    attr_input = dict()
+    user_inputs = []
+    for U_i, A_i in model.A.items():
+        if U_i == "Dimensionality of HPs":
+            user_inputs = custom_input(A_i, 2)
+        if U_i == "Conditional HP Space":
+            user_inputs = custom_input(A_i, 'no')
+        if U_i == "#continuous HPs of ML alg.":
+            user_inputs = custom_input(A_i, '0')
+        if U_i == "Number of possible function evaluations/maximum number of trials":
+            user_inputs = custom_input(A_i, '<50')
+        if U_i == "Machine Learning Algorithm":
+            user_inputs = custom_input(A_i, 'SVM')
+        if U_i == "Dataset to perform ML task":
+            user_inputs = custom_input(A_i, '16 OpenML classification datasets')
+        if U_i == "Artificial noise in dataset":
+            user_inputs = custom_input(A_i, 'no')
+        if U_i == "Surrogate Benchmarking":
+            user_inputs = custom_input(A_i, 'SVM')
+        if U_i == "Task that was performed by the ML algorithm who's HPs were optimized":
+            user_inputs = custom_input(A_i, 'Classification')
+
+        attr_input[U_i] = dict(zip(A_i, user_inputs))
+
+    X = AttributeInput(attr_input)
+    belief_degrees = model.run(X)
+    print(attr_input)
+    print(tuple(zip(model.D, belief_degrees)))
     '''
     # single values
     attr_input = dict()
