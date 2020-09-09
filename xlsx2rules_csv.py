233ae2b924e6747314905dbaca99bbf5c8c56f79
philippnoodt <philipp.noodt@ima-ifu.rwth-aachen.de>
2020-09-09 13:10:20 +0200

    xlsx2rules_csv.py: bugfix and replacing ',' with '.' in the belief table

--- xlsx2rules_csv.py
+++ xlsx2rules_csv.py
@@ -9,12 +9,17 @@ raw_rulebase = pd.read_csv(raw_filepath, sep=';')
 counter = 1
 antecedents = ['rule_id','rule_weight']
 test = raw_rulebase.isnull()
+
+# appending the antecedent names to the antecedents list
 while raw_rulebase.iloc[2, counter] != 'Rule ID':
     if pd.notnull(raw_rulebase.iloc[2, counter]):
             antecedents.append('A_' + raw_rulebase.iloc[2, counter])
     counter += 1
 
+# column id where the consequents start
 idx_ruleid = counter
+
+# creating a list with all consequent names
 counter += 1
 consequents = []
 while counter < raw_rulebase.shape[1]:
@@ -23,16 +28,28 @@ while counter < raw_rulebase.shape[1]:
     consequents.append('D_' + raw_rulebase.iloc[2, counter])
     counter += 1
 
-num_rules = 0
+#extract belief table from the xlsx file
 beliefs = raw_rulebase.iloc[3:, (idx_ruleid + 1):(idx_ruleid + 1 + len(consequents))].dropna(how='all')
+
 num_rules = beliefs.shape[0]
 
+#replace , with . in belief table for python float readability
+for col in range(beliefs.shape[1]):
+    for idx in range(beliefs.shape[0]):
+        value = beliefs.iloc[idx, col]
+        if pd.isna(value):
+            continue
+        elif ',' in value:
+            beliefs.iloc[idx, col] = value.replace(',', '.')
+
+# creating the rules dataframe which will be the final .csv
 rules = pd.DataFrame(index=range(num_rules), columns=(antecedents + consequents))
 
+# filling the ruleid column
 for num in range(num_rules):
     rules.iloc[num, 0] = num + 1
 
-
+# iterating through the antecedents to enter referential value in correct row (rule) in rules dataframe
 for ant in antecedents[2:]:
     col = 0
     while col < idx_ruleid:
@@ -41,15 +58,29 @@ for ant in antecedents[2:]:
             x = 1
             while pd.notnull(raw_rulebase.iloc[2+x, col]):
                 if pd.isnull(raw_rulebase.iloc[2+x, col+1]):
-                    break
+                    x += 1
                 elif isinstance(raw_rulebase.iloc[2+x, col+1], float):
                     ruleid = raw_rulebase.iloc[2+x, col+1]
                     rules[ant].iloc[int(ruleid) - 1] = raw_rulebase.iloc[2 + x, col]
                     break
                 else:
                     rule_list = raw_rulebase.iloc[2+x, col+1].split(',')
+                    rule_list = [int(elem) for elem in rule_list]
+
                     for ruleid in rule_list:
-                        rules[ant].iloc[int(ruleid)-1] = raw_rulebase.iloc[2 + x, col]
+                        rules[ant].iloc[ruleid - 1] = raw_rulebase.iloc[2 + x, col]
+                    '''
+                    data = []
+                    for idx in range(beliefs.shape[0]):
+                        if idx in rule_list:
+                            data.append(raw_rulebase.iloc[2 + x, col])
+                        else:
+                            data.append('')
+                        #ii = rules[ant]
+                        #iii = ruleid
+                        #rules[ant].iloc[int(ruleid)-1] = raw_rulebase.iloc[2 + x, col]
+                    rules[ant] = data
+                    '''
                     x += 1
         col += 1
 
